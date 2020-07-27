0a025d15911ae6b1ff8e2e1c71b8acf28f18d386
Waqar-107 <waqar.hassan866@gmail.com>
2020-07-27 23:17:11 +0600

    my groups fetched

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 # from dust i have come, dust i will be
 
-print("hello world")
+from get_my_groups import get_my_groups
+
+my_groups = get_my_groups()
+for i in range(len(my_groups)):
+    print(my_groups[i])
+
+print(len(my_groups))
