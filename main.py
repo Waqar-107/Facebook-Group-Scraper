d28ee7308f5d3544ae4f95a0fdbf531b9da328b6
Waqar-107 <waqar.hassan866@gmail.com>
2020-07-29 19:43:25 +0600

    upd

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ for i in range(len(my_groups)):
     if my_groups[i]["name"] == required_group_name:
         group_id = my_groups[i]["id"]
 
+print(group_id)
 get_group_post(group_id, required_group_name, 600, 100)
-# get_comments_of_group_post("1069932883202285_1089286034600303", required_group_name)
